d615bb36352eb612b406afcebad78e4d55d162f8
Surya-Behera <bbapuji866@gmail.com>
2024-07-15 01:05:51 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import Course from "./Course/course";
 import { Toaster } from "react-hot-toast";
 import Home from "./Home/home";
+import { BrowserRouter } from "react-router-dom";
+
 function App() {
   return (
     <>
       <div className="dark:bg-slate-900 dark:text-white">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/course" element={<Course/>} />
         </Routes>
+        </BrowserRouter>
         <Toaster />
       </div>
     </>
