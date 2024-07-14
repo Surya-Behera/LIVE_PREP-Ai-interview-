7d4217acec8848d9249a7c2cc868052306a84b5d
Surya-Behera <bbapuji866@gmail.com>
2024-07-14 21:27:00 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
 import { BrowserRouter as Router ,Routes,Route } from 'react-router-dom'
 import Home from './components/Home'
-import About from './components/About'
-import Header from './components/Header'
-import Trendpg from './components/Trendpg'
+
  
 
 function App() {
-  const handleSearch = (query) => {
-    // Implement your search logic here, for now, just log the query
-    console.log('Search query:', query);
-  };
+  
 
   return (
-    <div className='bg-red-200 min-h-screen  md:w-[1100px] w-[500px]'> 
+    <div > 
+  
     <Router>
-       <Header/>
       <Routes>
    
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/trendpg' element={<Trendpg/>}/>
+        <Route path='/' element={<Home/>}/>
       </Routes>
     </Router>
     </div>
